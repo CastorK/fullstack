43fe80bd6316e092fdc693d0cb0c1ea04a542666
Castor Köhler <castor.kohler@futurice.com>
2019-05-17 12:05:18 +0300

    2.5

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total';
 
 const Course = ({course}) => {
+    const total = course.parts.map( part => part.exercises ).reduce( (sum, part) => sum + part )
+
     return (
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <Total total={total} />
         </div>
     )
 }
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,34 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Course from './components/Course'
-import Total from './components/Total'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14
-      }
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 0
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 1
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 2
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        },
+        {
+            name: 'Node.js2',
+            id: 3,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
     ]
-  }
-  const total = course.parts.map( part => part.exercises ).reduce( (sum, part) => sum + part )
 
-  return (
-    <div>
-      <Course course={course} />
-      <Total total={total} />
-    </div>
-  )
+    return (
+        <div>{courses.map( course => <Course key={course.id} course={course} /> )}</div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
