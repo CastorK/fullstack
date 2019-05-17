16042efa02784f2d8a7b900e0dd6e2bcbd19e64d
Castor Köhler <castor.kohler@futurice.com>
2019-05-17 12:11:00 +0300

    Move App to separate file

---
+++ part2/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Course from './components/Course'
+
+
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 0
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 1
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 2
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        },
+        {
+            name: 'Node.js2',
+            id: 3,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+    return (
+        <div>{courses.map( course => <Course key={course.id} course={course} /> )}</div>
+    )
+}
+
+export default App
