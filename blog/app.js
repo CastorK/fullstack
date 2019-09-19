b79915c455bf9f7a244a02daf53e1856a210a82f
CastorK <castor.kohler@aalto.fi>
2019-09-19 16:00:20 +0300

    4.2 1h

---
+++ blog/app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.DB_URL
+console.log(`Connecting to ${mongoUrl}`)
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Successfully connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
