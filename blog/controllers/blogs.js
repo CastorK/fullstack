6ae21436be1777fa7eef49e2ed24b1ecfab46057
CastorK <castor.kohler@aalto.fi>
2019-10-31 15:43:31 +0200

    4.18 30min

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   response.json(await Blog.find({}).populate('user', {'username': 1, 'name': 1, 'id': 1}))
@@ -8,13 +17,22 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
+  const token = getTokenFrom(request)
+
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
     const users = await User.find({})
     const user = users[0]
     blog.user = user._id
     const savedBlog = await blog.save()
+
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
+
     response.status(201).json(savedBlog)
   } catch(exception) {
     next(exception)
